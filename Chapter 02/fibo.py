06fb8f546bd483be3026911732ce0c51216145c6
Shoyb Hasan <shoybhasan789@gmail.com>
2026-02-20 23:59:23 +0600

    Finished Chapter 02

---
+++ Chapter 02/fibo.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+    def find_fib(n):
+        if n <= 2:
+            return 1
+    
+        fib_x, fib_next = 1, 1
+    
+        i = 3
+        while i <=n:
+            i += 1
+            fib_x, fib_next = fib_next, fib_x + fib_next
+            return fib_next
+    
+    def list_fib(n):
+        fib_list = [1,1]
+        if n<= 2:
+            return fib_list[:n]
+        i = 3
+        while i <= n:
+            i += 1
+            fib_x, fib_next = fib_next, fib_x + fib_next
+            fib_list.append(fib_next)
+        return fib_list
+    
